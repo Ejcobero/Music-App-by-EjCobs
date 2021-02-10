5dd557632539fc0a9f3eadffe35f21cf8c2d4c75
Ejcobero <ejcobero@gmail.com>
2021-02-10 22:59:32 +0800

    Added webm music

--- src/app/components/Music/benand-ben/benand-ben.component.ts
+++ src/app/components/Music/benand-ben/benand-ben.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Howl } from 'howler';
+
+
+export interface Music  {
+  name: string;
+  path: string;
+};
 
 @Component({
   selector: 'app-benand-ben',
@@ -7,8 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenandBenComponent implements OnInit {
 
+  playlists: Music[] = [
+    {
+      name: 'Araw-Araw',
+      path: './assets/mp3/Ben&Ben-Araw-Araw.webm'
+    },
+    {
+      name: 'Lifetime',
+      path: './assets/mp3/Ben&Ben-Lifetime.webm'
+    },
+    {
+      name: 'Pagtingin',
+      path: './assets/mp3/Ben&Ben-Pagtingin.webm'
+    }
+  ];
+
+  activeMusic: Music = null;
+  player: Howl = null;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  start(music: Music) {
+    this.player = new Howl({
+      src: [this.playlists.path]
+    })
+  }
+
+  togglePlayer(pause) {
+
+  }
+
+  next() {
+
+  }
+
+  prev() {
+
+  }
+
+  seek() {
+
+  }
+
+  updateProgressBar() {
+
+  }
 }
