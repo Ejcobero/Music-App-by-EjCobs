ce6eee1a2523cf1323b0de40472a09298e049f4d
Ejcobero <ejcobero@gmail.com>
2021-02-11 20:48:54 +0800

    fix the main routes

--- src/app/pages/app/app.page.ts
+++ src/app/pages/app/app.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface Image  {
+  name: string;
+  path: string;
+  url: string;
+};
+
 
 @Component({
   selector: 'app-app',
@@ -9,15 +15,45 @@ import { Router } from '@angular/router';
 })
 export class AppPage implements OnInit {
 
+  pics: Image[] = [
+    {
+      name: 'Ben&Ben',
+      path: './assets/images/BenBen.jpg',
+      url: "/app/benand-ben-music"
+    },
+    {
+      name: 'December Avenue',
+      path: './assets/images/december-avenue.jpg',
+      url: "/app/december-avenue-music"
+    },
+    {
+      name: 'Ed Sheeran',
+      path: './assets/images/EdSheeran.jpg',
+      url: "/app/ed-sheeran-music"
+    },
+    {
+      name: 'I Belong to the Zoo',
+      path: './assets/images/IBelongToTheZoo.jpg',
+      url: "/app/ibelongtothe-zoo-music"
+    },
+    {
+      name: 'Moira Della Torre',
+      path: './assets/images/Moira.jpg',
+      url: "/app/moira-della-torre-music"
+    },
+    {
+      name: 'Olivia Rodrigo',
+      path: './assets/images/Olivia.jpg',
+      url: "/app/olivia-rodrigo-music"
+    },
 
+  ];
 
   constructor(private router: Router) { }
 
-  benben() {
-    this.router.navigateByUrl('/app/benand-ben-music');
-  }
 
   ngOnInit() {
+    this.router.navigate(['app']);
   }
 
 }
