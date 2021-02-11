8ee28192ef258bcd6fa9338e8729d93f0ab1a443
Ejcobero <ejcobero@gmail.com>
2021-02-11 20:46:23 +0800

    feature(benandbencomponent): Added the design and the logic for this component

--- src/app/components/Music/benand-ben/benand-ben.component.ts
+++ src/app/components/Music/benand-ben/benand-ben.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRange } from '@ionic/angular';
 import { Howl } from 'howler';
 
 
@@ -31,35 +32,84 @@ export class BenandBenComponent implements OnInit {
 
   activeMusic: Music = null;
   player: Howl = null;
+  isPlaying = false;
+  progress = 0;
+  @ViewChild('range') range: IonRange;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.start;
+    this.togglePlayer;
+    this.next;
+    this.prev;
+    this.seek;
+    this.updateProgressBar;
+  }
 
   start(music: Music) {
+    if (this.player) {
+      this.player.stop();
+    }
     this.player = new Howl({
-      src: this.playlists[index].path
+      src: [music.path],
+      html5: true,
+      onplay: () => {
+        console.log('onPlay');
+        this.isPlaying = true;
+        this.activeMusic = music;
+        this.updateProgressBar();
+      },
+      onend: () => {
+        console.log('onEnd');
+
+      }
     });
     this.player.play();
   }
 
   togglePlayer(pause) {
+    this.isPlaying = !pause;
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
 
   }
 
   next() {
+    let index = this.playlists.indexOf(this.activeMusic);
+    if (index != this.playlists.length - 1 ) {
+      this.start(this.playlists[index + 1]);
+    } else {
+      this.start(this.playlists[0]);
+    }
 
   }
 
   prev() {
+    let index = this.playlists.indexOf(this.activeMusic);
+    if (index > 0) {
+      this.start(this.playlists[index - 1]);
+    } else {
+      this.start(this.playlists[this.playlists.length - 1]);
+    }
 
   }
 
   seek() {
+    let newValue: any = this.range.value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
 
   }
 
   updateProgressBar() {
-
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgressBar();
+    }, 1000);
   }
 }
